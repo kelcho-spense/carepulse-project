b22b8e74c5bb8a46f7dd2dee9839761887d04a14
KevinComba <kelchospense88@gmail.com>
2024-08-21 21:15:22 +0300

    fixed onchange bug on react datepicker

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -93,7 +93,7 @@ const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
                         <ReactDatePicker
                             showTimeSelect={props.showTimeSelect ?? false}
                             selected={field.value}
-                            onChange={(date: Date) => field.onChange(date)}
+                            onChange={(date: Date | null, event?: React.SyntheticEvent<any> | undefined) => field.onChange(date)}
                             timeInputLabel="Time:"
                             dateFormat={props.dateFormat ?? "MM/dd/yyyy"}
                             wrapperClassName="date-picker"
