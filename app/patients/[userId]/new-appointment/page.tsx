d5872ee4f88a62f7c30f86d8510d43f4abbf9a21
KevinComba <kelchospense88@gmail.com>
2024-08-25 15:37:15 +0300

    success page done

---
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -0,0 +1,28 @@
+import AppointmentForm from "@/components/forms/AppointmentForm";
+import { getPatient } from "@/lib/actions/patient.actions";
+import Image from "next/image";
+
+
+export default async function Appointment({ params: { userId } }: SearchParamProps) {
+    const patient = await getPatient(userId);
+    return (
+        <div className="flex h-screen max-h-screen">
+            <section className="remove-scrollbar container my-auto">
+                <div className="sub-container max-w-[860px] flex-1 justify-between">
+                    <Image
+                        src="/assets/icons/logo-full.svg"
+                        height={1000}
+                        width={1000}
+                        alt="logo"
+                        className="mb-12 h-10 w-fit"
+                    />
+                    <AppointmentForm
+                        patientId={patient?.$id}
+                        userId={userId}
+                        type="create"
+                    />
+                </div>
+            </section>
+        </div>
+    )
+}
